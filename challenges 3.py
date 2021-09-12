bdc81e0a40c2158a7aafc3250380abd15c7c59e3
Marie-Anne1 <marieannemancinietrejuste00@gmail.com>
2021-09-11 23:52:14 -0400

    third branch

---
+++ challenges 3.py
@@ -0,0 +1,67 @@
+print("****************")
+print("Challenges 020")
+name = input("Enter your first name: ")
+length = len(name)
+print(length)
+print()
+
+print("****************")
+print("Challenges 021")
+firstname = input("Enter your first name: ")
+surname = input("Enter your surname: ")
+name = firstname + " " + surname
+length = len(name)
+print(name)
+print(length)
+print()
+
+print("****************")
+print("Challenges 022")
+firstname = input("Enter your first name in lowercase: ")
+surname = input("Enter your surname in lowercase: ")
+firstname = firstname.title()
+surname = surname.title()
+name = firstname + " " + surname
+print(name)
+print()
+
+print("****************")
+print("Challenges 023")
+phrase = input("Enter the first kine of a nursery rhyme: ")
+length = len(phrase)
+print("This has", length, "letters in it")
+start = int(input("Enter a starting number: "))
+end = int(input("Enter an end number: "))
+part = (phrase[start:end])
+print(part)
+print()
+
+print("****************")
+print("Challenges 024")
+word = input("Enter a word: ")
+word = word.upper()
+print(word)
+print()
+
+print("****************")
+print("Challenges 025")
+name = input("Enter your first name: ")
+if len(name)< 5:
+    surname = input("Enter your surname: ")
+    name = name+surname
+    print(name.upper())
+else:
+    print(name.lower())
+print()
+
+print("****************")
+print("Challenges 026")
+word = input("Please enter a word: ")
+first = word[0]
+length = len(word)
+rest = word[1:length]
+if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+    newword = rest + first + "ay"
+else:
+    newword = word + "way"
+print(newword.lower())
